14845cc94bbbaee7080b8bbed1512945d31b26a2
wshedden <will.sh@protonmail.com>
2022-07-16 03:48:01 +0100

    Change port message

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ var app = express();
 var server = app.listen(process.env.PORT || 3000, '0.0.0.0');
 
 app.use(express.static('public'));
-console.log("Server is running");
+console.log("Server is running on port " + process.env.PORT);
