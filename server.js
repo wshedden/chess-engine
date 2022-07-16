1eb9900e3125eb195e95752c5a9c211937fffbb6
wshedden <will.sh@protonmail.com>
2022-07-16 03:42:36 +0100

    Add port options

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
 var app = express();
-var server = app.listen(process.env.PORT);
+var server = app.listen(process.env.PORT || 3000, '0.0.0.0');
 
 app.use(express.static('public'));
 console.log("Server is running");
